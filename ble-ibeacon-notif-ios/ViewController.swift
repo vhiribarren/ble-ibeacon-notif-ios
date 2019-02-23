200e2481ef25a4d547ee606dffebc76671372578
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2019-02-23 08:44:27 +0100

    Initial Commit

---
+++ ble-ibeacon-notif-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ble-ibeacon-notif-ios
+//
+//  Created by Vincent Hiribarren on 23/02/2019.
+//  Copyright © 2019 Vincent Hiribarren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
